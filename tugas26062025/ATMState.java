ecb2d35897c0d778f38126b5a8af19794c0888b7
Muammaradami <adamimuammar@gmail.com>
2025-06-27 11:50:22 +0700

    Add files via upload

---
+++ tugas26062025/ATMState.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas26062025;
+
+/**
+ *
+ * @author Aspire Lite 14
+ */
+public class ATMState {
+    public static double saldo = 250000;
+}
